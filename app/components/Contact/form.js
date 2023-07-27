33e3537dc5db2fc92b35f3dd2786629986b256db
Steph T <wytseng@uci.edu>
2023-07-27 15:13:32 -0700

    commit new files left out on last commit

---
+++ app/components/Contact/form.js
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_nthx8fl', 'template_hqf31n4', form.current, 'ToYJ92mvbEoBFTcFW')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="form_container flex flex-col p-4">
+      <p className='font-poppins text-sm'>
+        get in touch with me through email: <a href="mailto:wytseng113@gmail.com" className='underline font-poppins'>wytsen113@gmail.com</a>
+      </p>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
